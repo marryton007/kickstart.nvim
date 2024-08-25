05350318b39332715e081bb0daea21031f3f1df5
liujiaxi <marryton007@gmail.com>
2024-08-25 20:52:08 +0800

    local modify

--- lua/custom/plugins/cmp.lua
+++ lua/custom/plugins/cmp.lua
@@ -98,6 +98,33 @@ return
             end
           end, { 'i', 's' }),
 
+        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expandable() then
+            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, {
+          'i',
+          's',
+        }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {
+            "i",
+            "s",
+          }),
           -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
           --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         },
--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,6 +1,13 @@
 return {
   'neovim/nvim-lspconfig',
-  config = function ()
-    require("lspconfig").ccls.setup {};
-  end
+  dependencies = { 'hrsh7th/cmp-nvim-lsp' },
+  config = function()
+    local capabilities = require('cmp_nvim_lsp').default_capabilities();
+    require('lspconfig').ccls.setup {
+      capabilities = capabilities,
+    }
+		require('lspconfig').svls.setup {
+      capabilities = capabilities,
+		}
+  end,
 }
