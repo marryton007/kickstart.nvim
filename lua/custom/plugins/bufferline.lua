69859bb67cfbaae2c38818069a3247e7e3bf36f2
liujiaxi <marryton007@gmail.com>
2024-08-25 22:09:11 +0800

    update init.lua from upstream

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
-  "akinsho/bufferline.nvim",
+  'akinsho/bufferline.nvim',
   dependencies = { 'folke/which-key.nvim' },
-  config = function ()
+  config = function()
     require('bufferline').setup {
       options = {
         numbers = function(opts)
@@ -13,20 +13,5 @@ return {
         max_name_length = 80,
       },
     }
-    require('which-key').register{
-      -- " 使用 <leader> [number] 切换到第 [number] 个 buffer
-      ["<leader>"] = {
-        ["1"] = { "<cmd>BufferLineGoToBuffer 1<cr>", "jump to buffer 1" },
-        ["2"] = { "<cmd>BufferLineGoToBuffer 2<cr>", "jump to buffer 2" },
-        ["3"] = { "<cmd>BufferLineGoToBuffer 3<cr>", "jump to buffer 3" },
-        ["4"] = { "<cmd>BufferLineGoToBuffer 4<cr>", "jump to buffer 4" },
-        ["5"] = { "<cmd>BufferLineGoToBuffer 5<cr>", "jump to buffer 5" },
-        ["6"] = { "<cmd>BufferLineGoToBuffer 6<cr>", "jump to buffer 6" },
-        ["7"] = { "<cmd>BufferLineGoToBuffer 7<cr>", "jump to buffer 7" },
-        ["8"] = { "<cmd>BufferLineGoToBuffer 8<cr>", "jump to buffer 8" },
-        ["9"] = { "<cmd>BufferLineGoToBuffer 9<cr>", "jump to buffer 9" },
-        ["0"] = { "<cmd>BufferLineGoToBuffer 10<cr>", "jump to buffer 10" },
-      }
-    }
-  end
+  end,
 }
--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,30 +1,7 @@
-return 
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ["g"] = {
-          d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "go to definition" },
-          r = { "<cmd>lua vim.lsp.buf.references()<cr>", "go to reference" },
-          w = { "<cmd>Telescope diagnostics<cr>", "diagnostics" },
-          i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "go to implementation" },
-          D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "go to declaration" },
-        },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
-  }
+return { -- Useful plugin to show you pending keybinds.
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require('which-key').setup()
+  end,
+}
