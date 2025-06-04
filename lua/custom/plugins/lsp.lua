77fa4657a828bb248f0d92413667c879532dbaa6
Niusan <niusan@ehomepoct.com>
2025-06-04 15:30:54 +0800

    merge from upstream

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -6,11 +6,12 @@ local servers = {
 }
 
 local lspconfig = require 'lspconfig'
-local cap = require('cmp_nvim_lsp').default_capabilities()
+local cap = require('blink.cmp').get_lsp_capabilities()
 
 for k, v in pairs(servers) do
   v.capabilities = vim.tbl_deep_extend('force', {}, cap, v.capabilities or {})
   lspconfig[k].setup(v)
 end
+
 
 return {}
