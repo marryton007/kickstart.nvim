2d264f242d8f6851f2ce775356912fb30d5168e8
Niusan <niusan@ehomepoct.com>
2025-10-10 18:02:22 +0800

    set fold configs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,4 +4,10 @@
 -- See the kickstart.nvim README for more information
 vim.opt.mouse = ''
 vim.g.have_nerd_font = true
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
 return {}
